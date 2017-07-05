942dca55049795c4f6dcb1d68748d84ef4eb7d4e
Ubuntu <mattshax@gmail.com>
2017-07-05 16:40:21 -0400

    removing old files

--- blenderRenderBackup/genScenes.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-Nscenes=19
-for scene in `seq 0 $Nscenes`
-do
-  echo $scene
-  sed "s/@num@/$scene/g" sceneOptions.txt > sceneOptions2.txt
-  blender -b --python renderScene.py 'sceneOptions2.txt'>log
-done
